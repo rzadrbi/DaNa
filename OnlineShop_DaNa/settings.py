0a0bac7d0f89c935c459154b913bfbbd1e9802af
RezaDarabi <rzadrbi@gmail.com>
2023-10-11 00:37:01 +0330

    template

--- OnlineShop_DaNa/settings.py
+++ OnlineShop_DaNa/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import os
+from os import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,11 +107,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
+    path.join(BASE_DIR, 'assets'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- OnlineShop_DaNa/urls.py
+++ OnlineShop_DaNa/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.conf.urls.static import static
 from . import settings
@@ -7,3 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
