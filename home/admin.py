63ac6b8cba22552c7b4a86390b089a666652d2b0
Reza <116594858+rzadrbi@users.noreply.github.com>
2024-08-07 20:43:08 +0330

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from home.models import contact_phone
 
 
+
+
 @admin.register(contact_phone)
 class contact_phoneAdmin(admin.ModelAdmin):
     list_display = ('phone', )
